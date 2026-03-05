ff7bda7e54411a4edf76f669282f70239538d52c
freigofas-sketch <freigofas@gmail.com>
2026-03-05 14:24:27 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function openVideo(){
+  document.getElementById("videoModal").classList.add("active")
+}
+
+function closeVideo(){
+  document.getElementById("videoModal").classList.remove("active")
+}
